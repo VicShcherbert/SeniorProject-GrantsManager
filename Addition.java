ba5ac39f84890094eefeeda340b57642d9474e13
alan-moss <amoss5841@gmail.com>
2022-04-12 15:16:46 -0700

    Update helloworld.java

---
+++ Addition.java
@@ -0,0 +1,12 @@
+class Main {
+
+    public static void main(String[] args) {
+      
+      int x = 3;
+      int y = 1;
+      
+      System.out.println(x + " " + y);
+      int sum = x + y;
+      System.out.println(sum);
+    }
+  }
