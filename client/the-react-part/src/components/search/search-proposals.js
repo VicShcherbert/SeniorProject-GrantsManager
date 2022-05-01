92f579460a777143a6438210059841cd851fd1d6
alan-moss <amoss5841@gmail.com>
2022-05-01 15:33:35 -0700

    New format, change to SQL queries. Now can search fields together, do not need exact match

---
+++ client/the-react-part/src/components/search/search-proposals.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import Axios from 'axios';
+import { Form } from 'semantic-ui-react';
+import '../../style.css';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+  Pagination,
+  Segment,
+  Header,
+  Button,
+  Modal,
+} from 'semantic-ui-react';
+import { UpdateModal } from '../modal/update-modal';
+
+export const Search = () => {
+  const [proposal_number, setPropNum] = useState('');
+  const [title, setTitle] = useState('');
+  const [investigator, setInvestigator] = useState('');
+  const [department_name, setDeptName] = useState('');
+  const [department_number, setDeptNum] = useState(0);
+
+  const [list, setList] = useState([]);
+  const [proposalsPerPage, setProposalsPerPage] = useState(30);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const search = () => {
+    Axios.post('http://localhost:3001/search', {
+        proposal_number: proposal_number,
+        title: title,
+        department_number: department_number,
+        department_name: department_name,
+        investigator: investigator,
+    }).then((response) => {
+      console.log(response.data);
+      setList(response.data); //because response contains 'data'
+    });
+  };
+
+  return (
+    <div>
+        <h2 id='page-title'>Search</h2>
+        <Form>
+        <div id='search-proposals'>
+          <div>
+            <label>Proposal Number: </label>
+            <input
+              type='text'
+              onChange={(event) => {
+                setPropNum(event.target.value);
+              }}
+            />
+
+            <label>Title: </label>
+            <input
+              type='text'
+              onChange={(event) => {
+                setTitle(event.target.value);
+              }}
+            />
+
+            <label>Investigator: </label>
+            <input
+              type='text'
+              onChange={(event) => {
+                setInvestigator(event.target.value);
+              }}
+            />
+
+            <label>Department Number: </label>
+            <input
+              type='number'
+              onChange={(event) => {
+                setDeptNum(event.target.value);
+              }}
+            />
+
+            <label>Department Name: </label>
+            <input
+              type='text'
+              onChange={(event) => {
+                setDeptName(event.target.value);
+              }}
+            />
+
+            <Form.Button onClick={search}> Search </Form.Button>
+          </div>
+        </div>
+      </Form>
+    </div>
+  );
+};
---
+++ client/the-react-part/src/components/search/search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Search } from './search-proposals';
+
+export const Search = () => {
+    
+    return(
+        <div>
+            <h2 id='page-title'>
+                Search for proposal
+            </h2>
+            <Search />
+        </div>
+    )
+}
