07abbfc009db06e9f89c4af0c5de106a29db2096
alan-moss <amoss5841@gmail.com>
2022-06-05 14:51:56 -0700

    Redid footer fix on search tab

--- client/the-react-part/src/components/footer/index.js
+++ client/the-react-part/src/components/footer/index.js
@@ -9,7 +9,7 @@ var d = new Date();
 var fullYear = d.getFullYear();
 
 export const Footer = () => (
-  <Segment textAlign='center' inverted color='grey'>
-    Office of Grants and Research Development | {fullYear}
-  </Segment>
+  <div class = 'footer'>
+    <Segment textAlign='center' inverted color='grey'>Office of Grants and Research Development | {fullYear}</Segment>
+  </div>
 );
